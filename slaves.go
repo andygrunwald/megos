26a0188e9904608e5572fb08a8045f316b44b38c
Andy Grunwald <andygrunwald@gmail.com>
2015-10-06 18:09:05 +0200

    Added docs to slave.go

--- slaves.go
+++ slaves.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 )
 
-func (c *Client) GetSlaveByID(state *State, slaveID string) (*Slave, error) {
-	for _, slave := range state.Slaves {
+// GetSlaveByID will return a a slave by its unique ID (slaveID).
+//
+// The list of slaves are provided by a state of a single node.
+func (c *Client) GetSlaveByID(slaves []Slave, slaveID string) (*Slave, error) {
+	for _, slave := range slaves {
 		if slaveID == slave.ID {
 			return &slave, nil
 		}

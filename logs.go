70e53f5b39bca1be0dd7708a34e853b2981f2d06
Andy Grunwald <andygrunwald@gmail.com>
2015-10-06 18:07:18 +0200

    Added docs for logs.go

--- logs.go
+++ logs.go
@@ -5,18 +5,27 @@ import (
 	"strconv"
 )
 
+// GetStdOutOfTask will return Stdout of a task.
+//
+// pid is a single mesos slave node.
+// directory is the directory of a single executor,
 func (c *Client) GetStdOutOfTask(pid *Pid, directory string) ([]byte, error) {
 	u := c.getBaseURLForLogs("stdout", pid, directory)
 	body, err := c.GetBodyOfHTTPResponse(&u)
 	return body, err
 }
 
+// GetStdErrOfTask will return Stdout of a task.
+//
+// pid is a single mesos slave node.
+// directory is the directory of a single executor,
 func (c *Client) GetStdErrOfTask(pid *Pid, directory string) ([]byte, error) {
 	u := c.getBaseURLForLogs("stderr", pid, directory)
 	body, err := c.GetBodyOfHTTPResponse(&u)
 	return body, err
 }
 
+// getBaseURLForLogs will build the URL to get Stdout or Stderr.
 func (c *Client) getBaseURLForLogs(mode string, pid *Pid, directory string) url.URL {
 	u := url.URL{
 		Scheme:   "http",

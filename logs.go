c60f3d75d35fe4b0e62d7350f88a97944a33d8a2
Andy Grunwald <andygrunwald@gmail.com>
2015-10-07 14:05:09 +0200

    Added TODO for https scheme

--- logs.go
+++ logs.go
@@ -28,6 +28,7 @@ func (c *Client) GetStdErrOfTask(pid *Pid, directory string) ([]byte, error) {
 // getBaseURLForLogs will build the URL to get Stdout or Stderr.
 func (c *Client) getBaseURLForLogs(mode string, pid *Pid, directory string) url.URL {
 	u := url.URL{
+		// TODO How to support https cluster?
 		Scheme:   "http",
 		Host:     pid.Host + ":" + strconv.Itoa(pid.Port),
 		Path:     "files/download.json",

996ce359ae5b8a3169ed1c4327a8d59ad6f30a08
Andy Grunwald <andygrunwald@gmail.com>
2015-10-10 10:38:18 +0200

    Added unit tests for slaves.go

--- slaves_test.go
+++ slaves_test.go
@@ -1,4 +1,40 @@
 package megos
 
-// Missing tests:
-// * GetSlaveByID
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGetSlaveByID_WithSlave(t *testing.T) {
+	setup()
+
+	slaveID := "Slave2"
+	slaves := []Slave{
+		Slave{ID: "Slave1"},
+		Slave{ID: "Slave2"},
+		Slave{ID: "Slave3"},
+	}
+
+	if s, err := client.GetSlaveByID(slaves, slaveID); !reflect.DeepEqual(s, &slaves[1]) {
+		t.Errorf("Slave is not the one as expected (%s). Expected %+v, got %+v", err, &slaves[1], s)
+	}
+}
+
+func TestGetSlaveByID_WithoutSlave(t *testing.T) {
+	setup()
+
+	slaveID := "Slave5"
+	slaves := []Slave{
+		Slave{ID: "Slave1"},
+		Slave{ID: "Slave2"},
+		Slave{ID: "Slave3"},
+	}
+
+	s, err := client.GetSlaveByID(slaves, slaveID)
+	if s != nil {
+		t.Errorf("Slave is not nil. Expected nil, got %+v", s)
+	}
+	if err == nil {
+		t.Errorf("err is not nil. Expected a string, got %s", err)
+	}
+}

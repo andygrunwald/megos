4cd63033bbed319202bd8306cea957924c997975
Andy Grunwald <andygrunwald@gmail.com>
2016-03-27 18:50:02 +0200

    Add a few more comments

--- types.go
+++ types.go
@@ -121,6 +121,7 @@ type Framework struct {
 	Labels           []Label    `json:"label"`
 }
 
+// Offer represents a single offer from a Mesos Slave to a Mesos master
 type Offer struct {
 	ID          string            `json:"id"`
 	FrameworkID string            `json:"framework_id"`
@@ -131,6 +132,7 @@ type Offer struct {
 	Attributes  map[string]string `json:"attributes"`
 }
 
+// URL represents a single URL
 type URL struct {
 	Scheme     string      `json:"scheme"`
 	Address    Address     `json:"address"`
@@ -138,17 +140,21 @@ type URL struct {
 	Parameters []Parameter `json:"parameters"`
 }
 
+// Address represents a single address.
+// e.g. from a Slave or from a Master
 type Address struct {
 	Hostname string `json:"hostname"`
 	IP       string `json:"ip"`
 	Port     int    `json:"port"`
 }
 
+// Parameter represents a single key / value pair for parameters
 type Parameter struct {
 	Key   string `json:"key"`
 	Value string `json:"value"`
 }
 
+// Label represents a single key / value pair for labeling
 type Label struct {
 	Key   string `json:"key"`
 	Value string `json:"value"`

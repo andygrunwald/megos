0a83d06ef8b481f976d4e8e582120de958921691
Andy Grunwald <andygrunwald@gmail.com>
2016-05-15 19:52:08 +0200

    Added missing structs for leader state

--- types.go
+++ types.go
@@ -30,71 +30,76 @@ type State struct {
 
 // Flags represents the flags of a mesos state
 type Flags struct {
-	AllocationInterval          string `json:"allocation_interval"`
-	Allocator                   string `json:"allocator"`
-	Authenticate                string `json:"authenticate"`
-	Authenticatee               string `json:"authenticatee"`
-	AuthenticateSlaves          string `json:"authenticate_slaves"`
-	Authenticators              string `json:"authenticators"`
-	Authorizers                 string `json:"authorizers"`
-	CgroupsEnableCfs            string `json:"cgroups_enable_cfs"`
-	CgroupsHierarchy            string `json:"cgroups_hierarchy"`
-	CgroupsLimitSwap            string `json:"cgroups_limit_swap"`
-	CgroupsRoot                 string `json:"cgroups_root"`
-	Cluster                     string `json:"cluster"`
-	ContainerDiskWatchInterval  string `json:"container_disk_watch_interval"`
-	Containerizers              string `json:"containerizers"`
-	DefaultRole                 string `json:"default_role"`
-	DiskWatchInterval           string `json:"disk_watch_interval"`
-	Docker                      string `json:"docker"`
-	DockerRemoveDelay           string `json:"docker_remove_delay"`
-	DockerSandboxDirectory      string `json:"docker_sandbox_directory"`
-	DockerStopTimeout           string `json:"docker_stop_timeout"`
-	EnforceContainerDiskQuota   string `json:"enforce_container_disk_quota"`
-	ExecutorRegistrationTimeout string `json:"executor_registration_timeout"`
-	ExecutorShutdownGracePeriod string `json:"executor_shutdown_grace_period"`
-	FrameworksHome              string `json:"frameworks_home"`
-	FrameworkSorter             string `json:"framework_sorter"`
-	GCDelay                     string `json:"gc_delay"`
-	GCDiskHeadroom              string `json:"gc_disk_headroom"`
-	HadoopHome                  string `json:"hadoop_home"`
-	Help                        string `json:"help"`
-	Hostname                    string `json:"hostname"`
-	InitializeDriverLogging     string `json:"initialize_driver_logging"`
-	IP                          string `json:"ip"`
-	Isolation                   string `json:"isolation"`
-	LauncherDir                 string `json:"launcher_dir"`
-	LogAutoInitialize           string `json:"log_auto_initialize"`
-	LogDir                      string `json:"log_dir"`
-	Logbufsecs                  string `json:"logbufsecs"`
-	LoggingLevel                string `json:"logging_level"`
-	MaxSlavePingTimeouts        string `json:"max_slave_ping_timeouts"`
-	Master                      string `json:"master"`
-	PerfDuration                string `json:"perf_duration"`
-	PerfInterval                string `json:"perf_interval"`
-	Port                        string `json:"port"`
-	Quiet                       string `json:"quiet"`
-	Quorum                      string `json:"quorum"`
-	Recover                     string `json:"recover"`
-	RecoverySlaveRemovalLimit   string `json:"recovery_slave_removal_limit"`
-	RecoveryTimeout             string `json:"recovery_timeout"`
-	RegistrationBackoffFactor   string `json:"registration_backoff_factor"`
-	Registry                    string `json:"registry"`
-	RegistryFetchTimeout        string `json:"registry_fetch_timeout"`
-	RegistryStoreTimeout        string `json:"registry_store_timeout"`
-	RegistryStrict              string `json:"registry_strict"`
-	ResourceMonitoringInterval  string `json:"resource_monitoring_interval"`
-	RootSubmissions             string `json:"root_submissions"`
-	SlavePingTimeout            string `json:"slave_ping_timeout"`
-	SlaveReregisterTimeout      string `json:"slave_reregister_timeout"`
-	Strict                      string `json:"strict"`
-	SwitchUser                  string `json:"switch_user"`
-	UserSorter                  string `json:"user_sorter"`
-	Version                     string `json:"version"`
-	WebuiDir                    string `json:"webui_dir"`
-	WorkDir                     string `json:"work_dir"`
-	ZK                          string `json:"zk"`
-	ZKSessionTimeout            string `json:"zk_session_timeout"`
+	AllocationInterval            string `json:"allocation_interval"`
+	Allocator                     string `json:"allocator"`
+	Authenticate                  string `json:"authenticate"`
+	AuthenticateHTTP              string `json:"authenticate_http"`
+	Authenticatee                 string `json:"authenticatee"`
+	AuthenticateSlaves            string `json:"authenticate_slaves"`
+	Authenticators                string `json:"authenticators"`
+	Authorizers                   string `json:"authorizers"`
+	CgroupsEnableCfs              string `json:"cgroups_enable_cfs"`
+	CgroupsHierarchy              string `json:"cgroups_hierarchy"`
+	CgroupsLimitSwap              string `json:"cgroups_limit_swap"`
+	CgroupsRoot                   string `json:"cgroups_root"`
+	Cluster                       string `json:"cluster"`
+	ContainerDiskWatchInterval    string `json:"container_disk_watch_interval"`
+	Containerizers                string `json:"containerizers"`
+	DefaultRole                   string `json:"default_role"`
+	DiskWatchInterval             string `json:"disk_watch_interval"`
+	Docker                        string `json:"docker"`
+	DockerRemoveDelay             string `json:"docker_remove_delay"`
+	DockerSandboxDirectory        string `json:"docker_sandbox_directory"`
+	DockerStopTimeout             string `json:"docker_stop_timeout"`
+	EnforceContainerDiskQuota     string `json:"enforce_container_disk_quota"`
+	ExecutorRegistrationTimeout   string `json:"executor_registration_timeout"`
+	ExecutorShutdownGracePeriod   string `json:"executor_shutdown_grace_period"`
+	FrameworksHome                string `json:"frameworks_home"`
+	FrameworkSorter               string `json:"framework_sorter"`
+	GCDelay                       string `json:"gc_delay"`
+	GCDiskHeadroom                string `json:"gc_disk_headroom"`
+	HadoopHome                    string `json:"hadoop_home"`
+	Help                          string `json:"help"`
+	Hostname                      string `json:"hostname"`
+	HostnameLookup                string `json:"hostname_lookup"`
+	HTTPAuthenticators            string `json:"http_authenticators"`
+	InitializeDriverLogging       string `json:"initialize_driver_logging"`
+	IP                            string `json:"ip"`
+	Isolation                     string `json:"isolation"`
+	LauncherDir                   string `json:"launcher_dir"`
+	LogAutoInitialize             string `json:"log_auto_initialize"`
+	LogDir                        string `json:"log_dir"`
+	Logbufsecs                    string `json:"logbufsecs"`
+	LoggingLevel                  string `json:"logging_level"`
+	MaxCompletedFrameworks        string `json:"max_completed_frameworks"`
+	MaxCompletedTasksPerFramework string `json:"max_completed_tasks_per_framework"`
+	MaxSlavePingTimeouts          string `json:"max_slave_ping_timeouts"`
+	Master                        string `json:"master"`
+	PerfDuration                  string `json:"perf_duration"`
+	PerfInterval                  string `json:"perf_interval"`
+	Port                          string `json:"port"`
+	Quiet                         string `json:"quiet"`
+	Quorum                        string `json:"quorum"`
+	Recover                       string `json:"recover"`
+	RecoverySlaveRemovalLimit     string `json:"recovery_slave_removal_limit"`
+	RecoveryTimeout               string `json:"recovery_timeout"`
+	RegistrationBackoffFactor     string `json:"registration_backoff_factor"`
+	Registry                      string `json:"registry"`
+	RegistryFetchTimeout          string `json:"registry_fetch_timeout"`
+	RegistryStoreTimeout          string `json:"registry_store_timeout"`
+	RegistryStrict                string `json:"registry_strict"`
+	ResourceMonitoringInterval    string `json:"resource_monitoring_interval"`
+	RootSubmissions               string `json:"root_submissions"`
+	SlavePingTimeout              string `json:"slave_ping_timeout"`
+	SlaveReregisterTimeout        string `json:"slave_reregister_timeout"`
+	Strict                        string `json:"strict"`
+	SwitchUser                    string `json:"switch_user"`
+	UserSorter                    string `json:"user_sorter"`
+	Version                       string `json:"version"`
+	WebuiDir                      string `json:"webui_dir"`
+	WorkDir                       string `json:"work_dir"`
+	ZK                            string `json:"zk"`
+	ZKSessionTimeout              string `json:"zk_session_timeout"`
 }
 
 // Framework represent a single framework of a mesos node
@@ -107,6 +112,7 @@ type Framework struct {
 	Hostname         string     `json:"hostname"`
 	ID               string     `json:"id"`
 	Name             string     `json:"name"`
+	PID              string     `json:"pid"`
 	OfferedResources Resources  `json:"offered_resources"`
 	Offers           []Offer    `json:"offers"`
 	RegisteredTime   float64    `json:"registered_time"`
@@ -119,6 +125,8 @@ type Framework struct {
 	User             string     `json:"user"`
 	WebuiURL         string     `json:"webui_url"`
 	Labels           []Label    `json:"label"`
+	// Missing fields
+	// TODO: "capabilities": [],
 }
 
 // Offer represents a single offer from a Mesos Slave to a Mesos master
@@ -164,14 +172,33 @@ type Label struct {
 type Task struct {
 	// Missing fields
 	// TODO: "labels": [],
-	ExecutorID  string       `json:"executor_id"`
-	FrameworkID string       `json:"framework_id"`
-	ID          string       `json:"id"`
-	Name        string       `json:"name"`
-	Resources   Resources    `json:"resources"`
-	SlaveID     string       `json:"slave_id"`
-	State       string       `json:"state"`
-	Statuses    []TaskStatus `json:"statuses"`
+	ExecutorID  string        `json:"executor_id"`
+	FrameworkID string        `json:"framework_id"`
+	ID          string        `json:"id"`
+	Name        string        `json:"name"`
+	Resources   Resources     `json:"resources"`
+	SlaveID     string        `json:"slave_id"`
+	State       string        `json:"state"`
+	Statuses    []TaskStatus  `json:"statuses"`
+	Discovery   TaskDiscovery `json:"discovery"`
+}
+
+// TaskDiscovery represents the dicovery information of a task
+type TaskDiscovery struct {
+	Visibility string `json:"visibility"`
+	Name       string `json:"name"`
+	Ports      Ports  `json:"ports"`
+}
+
+// Ports represents a number of PortDetails
+type Ports struct {
+	Ports []PortDetails `json:"ports"`
+}
+
+// PortDetails represents details about a single port
+type PortDetails struct {
+	Number   int    `json:"number"`
+	Protocol string `json:"protocol"`
 }
 
 // Resources represents a resource type for a task
@@ -184,19 +211,41 @@ type Resources struct {
 
 // TaskStatus represents the status of a single task
 type TaskStatus struct {
-	State     string  `json:"state"`
-	Timestamp float64 `json:"timestamp"`
+	State           string          `json:"state"`
+	Timestamp       float64         `json:"timestamp"`
+	ContainerStatus ContainerStatus `json:"container_status"`
+}
+
+// ContainerStatus represents the status of a single container inside a task
+type ContainerStatus struct {
+	NetworkInfos []NetworkInfo `json:"network_infos"`
+}
+
+// NetworkInfo represents information about the network of a container
+type NetworkInfo struct {
+	IpAddress   string      `json:"ip_address"`
+	IpAddresses []IpAddress `json:"ip_addresses"`
+}
+
+// IpAddress represents a single IpAddress
+type IpAddress struct {
+	IpAddress string `json:"ip_address"`
 }
 
 // Slave represents a single mesos slave node
 type Slave struct {
-	Active         bool              `json:"active"`
-	Hostname       string            `json:"hostname"`
-	ID             string            `json:"id"`
-	PID            string            `json:"pid"`
-	RegisteredTime float64           `json:"registered_time"`
-	Resources      Resources         `json:"resources"`
-	Attributes     map[string]string `json:"attributes"`
+	Active              bool              `json:"active"`
+	Hostname            string            `json:"hostname"`
+	ID                  string            `json:"id"`
+	PID                 string            `json:"pid"`
+	RegisteredTime      float64           `json:"registered_time"`
+	Resources           Resources         `json:"resources"`
+	UsedResources       Resources         `json:"used_resources"`
+	OfferedResources    Resources         `json:"offered_resources"`
+	ReservedResources   Resources         `json:"reserved_resources"`
+	UnreservedResources Resources         `json:"unreserved_resources"`
+	Attributes          map[string]string `json:"attributes"`
+	Version             string            `json:"version"`
 }
 
 // Executor represents a single executor of a framework
